a4cee695d9e161027b644754d73b6d53c9682e43
TheNarcissist1 <unreal810@gmail.com>
2022-10-09 00:22:08 +0100

    changes

---
+++ app.js
@@ -0,0 +1,32 @@
+const formSubmitBtn = document.getElementById("formSubmitBtn");
+const formInput = document.getElementById("formInput");
+const formInputOne = document.getElementById("formInputOne");
+const formInputTwo = document.getElementById("formInputTwo");
+
+let loginArr = [];
+
+formSubmitBtn.addEventListener("click", (e) => {
+    const defaultUser = "dontcallkickbuttbenjiat3am";
+    const defaultPass = "heheheha";
+
+    formValidation();
+    e.preventDefault();
+
+    formInputOne.value === defaultUser ? loginArr.push(defaultUser) : null;
+    formInputTwo.value === defaultPass ? loginArr.push(defaultPass) : null;
+    
+   loginArr.includes(defaultUser && defaultPass) ? location.href = "landing.html" : null;
+    
+});
+
+function formValidation() {
+    if (formInputOne.value === "" || formInputTwo.value === "") {
+        formInputOne.classList.add("is-invalid");
+        formInputTwo.classList.add("is-invalid");
+    }
+    
+    else {
+        formInputOne.classList.remove("is-invalid");
+        formInputTwo.classList.remove("is-invalid");
+    };
+};
