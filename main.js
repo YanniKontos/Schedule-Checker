403818a508afa5458da8a4f1b96038bce790cd35
TheNarcissist1 <unreal810@gmail.com>
2022-10-09 23:10:23 +0100

    changes

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-function returnHome() {
-    location.href = "index.html";
+const numericalDays = document.querySelectorAll(".days");
+const reverseMonth = document.getElementById("reverseMonth");
+const forwardMonth = document.getElementById("forwardMonth");
+
+reverseMonth.addEventListener("click", previousMonth);
+forwardMonth.addEventListener("click", nextMonth);
+
+function previousMonth(){
+    console.log('hello');
 };
+
+function nextMonth() {
+    console.log("hello2");
+};
+
+numericalDays.forEach(days => {
+    days.addEventListener("click", (e) => {
+        console.log(e.target.textContent);
+    });
+});
